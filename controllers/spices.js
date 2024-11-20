4780b24e64f1b863fd34d98b5936713404e5adf0
Sri Vidya <svvilipala@gmail.com>
2024-11-19 18:42:41 -0600

    Added spice create and updated

--- controllers/spices.js
+++ controllers/spices.js
@@ -1,6 +1,6 @@
 // controllers/spice.js
-var Spice = require('../models/spices');
- 
+var Spice = require('../models/spices'); 
+
 // List of all Spices
 exports.spice_list = async function(req, res) {
     try {
@@ -10,18 +10,18 @@ exports.spice_list = async function(req, res) {
         res.status(500).send({"error": `${err}`});
     }
 };
- 
+
 // View all Spices
 exports.spice_view_all_Page = async function(req, res) {
     try {
-        const theSpices = await Spice.find();
+        const theSpices = await Spice.find(); 
         res.render('spices', { title: 'Spice Search Results', results: theSpices });
     } catch (err) {
         res.status(500);
         res.send({"error": `${err}`});
     }
 };
- 
+
 // Handle Spice creation on POST
 exports.spice_create_post = async function(req, res) {
     console.log(req.body);
@@ -38,9 +38,9 @@ exports.spice_create_post = async function(req, res) {
         res.send({"error": `${err}`});
     }
 };
- 
- 
- 
+
+
+
    // Get details of a specific Spice
    exports.spice_detail = async function(req, res) {
     console.log("detail" + req.params.id); // Log the ID to verify
@@ -55,7 +55,7 @@ exports.spice_create_post = async function(req, res) {
 };
 //     res.send('NOT IMPLEMENTED: Spice detail: ' + req.params.id);
 // };
- 
+
 // Handle Spice delete on DELETE
 exports.spice_delete = async function(req, res) {
     console.log("delete" + req.params.id)
@@ -68,9 +68,9 @@ exports.spice_delete = async function(req, res) {
         res.send(`{"error":Error deleting ${err}}`);
     }
 };
- 
- 
- 
+
+
+
 // Handle Spice update on PUT
 exports.spice_update_put = async function(req, res) {
     console.log(`update on id${req.params.id}with body ${JSON.stringify(req.body)}`)
@@ -89,8 +89,8 @@ exports.spice_update_put = async function(req, res) {
         res.send(`{"error":${err}: Update for id ${req.params.id}failed`);
     }
     };
- 
-   
+
+    
 // Handle a show one view with id specified by query
 exports.spice_view_one_Page = async function(req, res) {
     console.log("single view for id " + req.query.id); // Log the ID to debug
@@ -107,4 +107,29 @@ exports.spice_view_one_Page = async function(req, res) {
 };
 
 
+// Handle building the view for creating a spice.
+// No body, no in-path parameter, no query.
+// Does not need to be async
+exports.spice_create_Page = function (req, res) {
+    console.log("create view");
+    try {
+        res.render('spicecreate', { title: 'spice Create' });
+    } catch (err) {
+        res.status(500).send(`{'error': '${err}'}`);
+    }
+};
+
+
+// Handle building the view for updating a spice.
+// Query provides the id
+
+exports.spice_update_Page = async function (req, res) {
+    console.log("update view for item " + req.query.id);
+    try {
+        let result = await Spice.findById(req.query.id);
+        res.render('spiceupdate', { title: 'spice Update', toShow: result });
+    } catch (err) {
+        res.status(500).send(`{'error': '${err}'}`);
+    }
+};
  
--- routes/spices.js
+++ routes/spices.js
@@ -1,55 +1,3 @@
-// var express = require('express');
-// var spices_controlers = require('../controllers/spices');
-// var router = express.Router();
-
-// // GET request for all spices
-// router.get('/', spices_controlers.spice_view_all_Page);
-
-// // GET request for one spice detail
-// router.get('/:id', spices_controlers.spice_detail);
-
-// // POST request for creating a spice
-// router.post('/', spices_controlers.spice_create_post);
-
-// // PUT request to update a spice
-// router.put('/:id', spices_controlers.spice_update_put);
-
-// // DELETE request to delete a spice
-// router.delete('/:id', spices_controlers.spice_delete);
-
-// module.exports = router;
-
-
-// 
-
-// var express = require('express');
-// var router = express.Router();
-// var Spice = require('../models/spices'); // Import the Spice model
-
-// // Spice Search Results page
-// router.get('/', function(req, res, next) {
-//     Spice.find({}, function(err, spices) {  // Find all spices in the collection
-//         if (err) {
-//             return next(err); // Handle errors
-//         }
-//         res.render('spices', {
-//             title: 'Spice Search Results',
-//             spices: spices  // Pass the spices data to the view
-//         });
-//     });
-// });
-
-// module.exports = router;
-
-// var express = require('express');
-// var spice_controlers = require('../controllers/spices'); 
-// var router = express.Router();
- 
-// /* GET spices page. */
-// router.get('/', spice_controlers.spice_view_all_Page); 
- 
-// // router.get('/:id', spice_controlers.spice_detail); 
-// module.exports = router;
 
 var express = require('express');
 var spice_controlers = require('../controllers/spices');
@@ -57,23 +5,28 @@ var router = express.Router();
  
 /* GET spices page. */
 router.get('/', spice_controlers.spice_view_all_Page);
-
  
 //router.get('/spices/:id', spice_controlers.spice_detail);
 router.get('/spices/:id', spice_controlers.spice_detail);
- 
+
  
 // POST route for creating a new spice
 router.post('/', spice_controlers.spice_create_post);
 router.put('/:id', spice_controlers.spice_update_put);
  
-router.delete('/:id', spice_controlers.spice_delete);
- 
- 
+router.delete('/:id', spice_controlers.spice_delete); 
+
+
 /* GET detail costume page */
 router.get('/detail', spice_controlers.spice_view_one_Page);
 
+router.get('/create', spice_controlers.spice_create_Page);
+
+/* GET update costume page */
+router.get('/update', spice_controlers.spice_update_Page);
+
+/* GET delete costume page */
+//router.get('/delete', spice_controlers.spice_delete_Page);
 
 
- 
 module.exports = router;
