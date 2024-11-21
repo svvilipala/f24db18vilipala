c48f083838c6cf234d0fe6aa3f166a0f6d738986
Sri Vidya <svvilipala@gmail.com>
2024-11-21 16:09:10 -0600

    Added Passport

--- app.js
+++ app.js
@@ -3,6 +3,25 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy
+
+passport.use(new LocalStrategy(function(username, password, done) {
+  Account.findOne({ username: username }).then(function (user){
+    if (err) { 
+      return done(err);
+    }
+    if (!user) {
+      return done(null, false, { message: 'Incorrect username.' });
+    }
+    if (!user.validPassword(password)) {
+      return done(null, false, { message: 'Incorrect password.' });
+    }
+    return done(null, user);
+  }).catch(function(err){
+    return done(err)
+  })
+}))
 
 
 require('dotenv').config();
@@ -36,6 +55,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 
@@ -95,6 +122,13 @@ app.use(bodyParser.urlencoded({extended: false}));
 var resourceRouter = require('./routes/resource');
 app.use('/resource', resourceRouter);
 
+// passport config
+// Use the existing connection
+// The Account model 
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 // Catch 404 and forward to error handler
 app.use(function(req, res, next) {
